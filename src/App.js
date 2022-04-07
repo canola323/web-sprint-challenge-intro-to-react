b2d4e03e75c924f859283ac0f5fa789ed3e6635f
Jonathan Canola <jac05@MacBook-Pro.attlocal.net>
2022-04-06 20:21:08 -0700

    solved all tasked

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
+ 
+const CardContainer = styled.ul`
+width: 80%;
+margin: 0 auto;
+`
+ 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+ 
+  const [characters, setCharacters] = useState([]);
+ 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      setCharacters(res.data);
+    }).catch(err => console.error(err))
+  }, [])
+ 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+ 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CardContainer>
+        {characters.map(ch => {
+          return  <Character key={ch.id} data={ch}/>
+        })}
+      </CardContainer>
     </div>
   );
 }
-
+ 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+
+import React, { useState } from "react";
+import styled from "styled-components";
+ 
+const StyledCard= styled.div`
+width: 250px;
+height: 300px;
+border: 5px solid black;
+display: inline-grid;
+margin: 20px;
+background-color: white;
+`
+const CharacterName= styled.h2`
+font-family: 'Coda';
+font-size: 2em;
+`
+ 
+const CharacterInfo= styled.p`
+font-family: 'Oswald';
+font-size: 1.6em;
+font-weight: 300;
+`
+ 
+function Character(props) {
+  const {data} = props;
+  return (
+    <StyledCard>
+      <CharacterName>{data.name}</CharacterName>
+      <CharacterInfo>Birth Year: {data.birth_year}</CharacterInfo>
+      <CharacterInfo>Gender: {data.gender}</CharacterInfo>
+    </StyledCard>
+  )
+}
+ 
+export default Character
+
